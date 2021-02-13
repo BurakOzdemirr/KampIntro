a1ab517361da131ec01ab5a5aabce68ced11134d
Burak Ozdemir <burak.ozdemirdir@gmail.com>
2021-02-14 00:31:36 +0300

    Software developer breeding camp codes added

---
+++ Metotlar/Urun.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Urun
+    {
+       
+      
+                  //Property - özellik
+                public int Id { get; set; }
+                public string Adi { get; set; }
+                public double Fiyati { get; set; }
+                public string Aciklama { get; set; }
+                public int StokAdedi { get; set; }
+
+    }
+}
+
